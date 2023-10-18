eb39997dd19b9f5f6eb52f53d7a3338f51c10512
sebasizq <sebastiansaavedra_2005@hotmaail.com>
2023-10-18 14:21:53 -0500

    asignacion de funciones

--- vectores/matrizDispersa.h
+++ vectores/matrizDispersa.h
@@ -37,34 +37,34 @@ public:
      DisperseMatrix();
      DisperseMatrix(int **&matriz, int m, int n); // Arreglo de 2 dimensiones
      DisperseMatrix( const vector<vector<int>> &matriz , int m , int n); // Vector de Vectores 
-     DisperseMatrix(DisperseMatrix &matriz);      // Punto 3
+     DisperseMatrix(DisperseMatrix &matriz);      // Punto 3 --J
 
     /* Modificadoras */
-    void rebuild(DisperseMatrix &matriz); // Reconstruir la matriz dispersa a normal con ceros
-    void assign(int i, int j, int v);     // Modificar el valor en la posición i, j
-    void add(DisperseMatrix &matriz);     // Suma entre matrices
-    void productVector(vector<int> vec);  // Multiplica el objeto actual por el vector
+    void rebuild(DisperseMatrix &matriz); // Reconstruir la matriz dispersa a normal con ceros --S
+    void assign(int i, int j, int v);     // Modificar el valor en la posición i, j --J
+    void add(DisperseMatrix &matriz);     // Suma entre matrices --S
+    void productVector(vector<int> vec);  // Multiplica el objeto actual por el vector --J
 
     /* Analizadoras */
-    void get(int i, int j);            // Obtener valor i, j
-    Lista getRowLis(int fila);            // Retornar fila
-    vector<int> getRowVec(int fila); 
-    Lista getColLis(int columna);         // Retornar columna
-    vector<int> getColVec(int columna); 
-    Lista getDisperseRowLis(int fila);    // Retornar fila con ceros
-    vector<int> getDisperseRowVec(int fila);
-    Lista getDisperseColLis(int columna); // Retornar columna con ceros
-    vector<int> getDisperseColVec(int columna);
-    void printMatrix(string sep);      // Imprime la matriz con el separador
-    int getMax();                      // Retorna el mayor elemento de la matriz
-    DisperseMatrix getTranspose();     // Transpone la matriz
-    Lista addMatrixList(DisperseMatrix matriz);
-    vector<int> addMatrixListVec(DisperseMatrix matriz);
+    void get(int i, int j);            // Obtener valor i, j --S
+    Lista getRowLis(int fila);            // Retornar fila -- J
+    vector<int> getRowVec(int fila);    //--S
+    Lista getColLis(int columna);         // Retornar columna --J
+    vector<int> getColVec(int columna); // --S
+    Lista getDisperseRowLis(int fila);    // Retornar fila con ceros -- J
+    vector<int> getDisperseRowVec(int fila); // --S
+    Lista getDisperseColLis(int columna); // Retornar columna con ceros --J
+    vector<int> getDisperseColVec(int columna); // --J
+    void printMatrix(string sep);      // Imprime la matriz con el separador --S
+    int getMax();                      // Retorna el mayor elemento de la matriz --J
+    DisperseMatrix getTranspose();     // Transpone la matriz --S
+    Lista addMatrixList(DisperseMatrix matriz); // --J
+    vector<int> addMatrixListVec(DisperseMatrix matriz); // --S
 
     /* Sobrecarga operadores */
-    DisperseMatrix operator+(DisperseMatrix &matriz); // Suma entre 2 matrices
-    DisperseMatrix operator*(DisperseMatrix &matriz); // Multiplicación entre 2 matrices
-    bool operator==(DisperseMatrix &matriz);          // Verifica si 2 matrices son iguales
+    DisperseMatrix operator+(DisperseMatrix &matriz); // Suma entre 2 matrices --J
+    DisperseMatrix operator*(DisperseMatrix &matriz); // Multiplicación entre 2 matrices --S
+    bool operator==(DisperseMatrix &matriz);          // Verifica si 2 matrices son iguales --J
 };
 
 #endif
