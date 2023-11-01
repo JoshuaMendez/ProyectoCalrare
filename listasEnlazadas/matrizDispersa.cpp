945741b9f5e6497c195712ed8400991b9dad0c1e
Joshua Mendez <joshuamendez0501@gmail.com>
2023-10-31 23:18:31 -0500

    add constructor empty, vec<vec> & rebuild

--- listasEnlazadas/matrizDispersa.cpp
+++ listasEnlazadas/matrizDispersa.cpp
@@ -1,3 +1,95 @@
 #include "matrizDispersa.h"
 
 /* Constructoras */
+
+DisperseMatrix::DisperseMatrix()
+{
+    matriz;
+    nColumnas = 0;
+}
+
+DisperseMatrix::DisperseMatrix(const vector<vector<int>> &vec, int m, int n)
+{
+    matriz.resize(m);
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (vec[i][j] != 0)
+            {
+                pair<int, int> par;
+                par.first = vec[i][j];
+                par.second = j;
+                matriz[i].push_back(par);
+            }
+        }
+        cout << endl;
+    }
+    cout << endl;
+    nFilas = m;
+    nColumnas = n;
+}
+
+/* --- Constructoras --- */
+
+/* Modificadoras */
+
+vector<vector<int>> DisperseMatrix::rebuild()
+{
+    vector<vector<int>> vec;
+    list<pair<int, int>>::iterator it;
+
+    for (int i = 0; i < nFilas; i++)
+    {
+        vec.push_back(vector<int>());
+        it = matriz[i].begin();
+
+        for (int j = 0; j < nColumnas; j++)
+        {
+            int tmp = 0;
+            if (it->second == j)
+            {
+                tmp = it->first;
+                it++;
+            }
+            vec[i].push_back(tmp);
+        }
+    }
+    // for (int i = 0; i < nFilas; i++)
+    // {
+    //     for (int j = 0; j < nColumnas; j++)
+    //     {
+    //         cout << vec[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+    return vec;
+}
+
+void DisperseMatrix::add(DisperseMatrix &matriz2)
+{
+    // Podría ir fila por fila verificando la columna y si esa fila y columna también existe en la matriz2,
+    // sumarle eso, sino, sumar 0
+    
+    for (int fila = 0; fila < nFilas; fila++)
+    {
+        cout << "Fila " << fila << ": ";
+        for (const pair<int, int> &elemento : matriz[fila])
+        {
+            cout << "(" << elemento.first << ", " << elemento.second << ") ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    for (int fila = 0; fila < matriz2.nFilas; fila++)
+    {
+        cout << "Fila " << fila << ": ";
+        for (const pair<int, int> &elemento : matriz2.matriz[fila])
+        {
+            cout << "(" << elemento.first << ", " << elemento.second << ") ";
+        }
+        cout << endl;
+    }
+}
+
+/* --- Modificadoras --- */
--- listasEnlazadas/matrizDispersa.h
+++ listasEnlazadas/matrizDispersa.h
@@ -23,39 +23,40 @@ using namespace std;
 
 class DisperseMatrix
 {
-private:
-    vector<list<pair<int, int>>> matriz ;
-    int nFilas;
-    int nColumnas;
+    // private:
 public:
+    vector<list<pair<int, int>>> matriz;
+    int nFilas; // Equivalente a tamaño del vector
+    int nColumnas;
+    // public:
     /************************
      * OPERACIONES DEL TAD *
      ************************/
- /* Constructoras */
-    DisperseMatrix();                                                // Vacío --J
-    DisperseMatrix(int **&matriz, int m, int n);                     // Arreglo de 2 dimensiones --S
-    DisperseMatrix(const vector<vector<int>> &matriz, int m, int n); // Vector de Vectores  --J
-    DisperseMatrix(DisperseMatrix &matriz);                          // Punto 3 --S
+    /* Constructoras */
+    DisperseMatrix();                                             // Vacío --ready
+    DisperseMatrix(int **&matriz, int m, int n);                  // Arreglo de 2 dimensiones --S
+    DisperseMatrix(const vector<vector<int>> &vec, int m, int n); // Vector de Vectores  --ready
+    DisperseMatrix(DisperseMatrix &matriz);                       // Punto 3 --S
 
     /* Modificadoras */
-    vector<vector<int>> rebuild();        // Reconstruir la matriz dispersa a normal con ceros y la retonar --J
+    vector<vector<int>> rebuild();        // Reconstruir la matriz dispersa a normal con ceros y la retonar --ready
     void assign(int i, int j, int v);     // Modificar el valor en la posición i, j --S
-    void add(DisperseMatrix &matriz);     // Suma entre matrices --J
+    void add(DisperseMatrix &matriz2);    // Suma entre matrices --J
     void productVector(vector<int> &vec); // Multiplica el objeto actual por el vector --S
 
     /* Analizadoras */
-    int get(int i, int j);                           // Obtener valor i, j --J
-    list<int> getRowLis(int fila);                   // Retornar fila --S
-    vector<int> getRowVec(int fila);                 // Retornar fila --J
-    list<int> getColLis(int columna);                // Retornar columna --S
-    vector<int> getColVec(int columna);              //  Retornar columna --J
-    list<int> getDisperseRowLis(int fila);           // Retornar fila con ceros --S
-    vector<int> getDisperseRowVec(int fila);         // Retornar una fila con ceros --J
-    list<int> getDisperseColLis(int columna);        // Retornar columna con ceros --S
-    vector<int> getDisperseColVec(int columna);      // --J
-    void printMatrix(string sep);                    // Imprime la matriz con el separador --S
-    int getMax();                                    // Retorna el mayor elemento de la matriz --J
-    DisperseMatrix getTranspose();                   // Transpone la matriz --S
+    int get(int i, int j);                                      // Obtener valor i, j --J
+    list<int> getRowLis(int fila);                              // Retornar fila --S
+    vector<int> getRowVec(int fila);                            // Retornar fila --J
+    list<int> getColLis(int columna);                           // Retornar columna --S
+    vector<int> getColVec(int columna);                         //  Retornar columna --J
+    list<int> getDisperseRowLis(int fila);                      // Retornar fila con ceros --S
+    vector<int> getDisperseRowVec(int fila);                    // Retornar una fila con ceros --J
+    list<int> getDisperseColLis(int columna);                   // Retornar columna con ceros --S
+    vector<int> getDisperseColVec(int columna);                 // --J
+    void printMatrix(string sep);                               // Imprime la matriz con el separador --S
+    int getMax();                                               // Retorna el mayor elemento de la matriz --J
+    DisperseMatrix getTranspose();                              // Transpone la matriz --S
     DisperseMatrix addMatrixList(list<DisperseMatrix> &matriz); // --J
 
     /* Sobrecarga operadores */
@@ -65,4 +66,3 @@ public:
 };
 
 #endif
-
