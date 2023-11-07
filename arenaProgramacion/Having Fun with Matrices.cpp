dcdf972de5fe9259c51bf9fbe4a597d371501329
Joshua Mendez <joshuamendez0501@gmail.com>
2023-11-07 13:27:42 -0500

    UVa problems

---
+++ arenaProgramacion/Having Fun with Matrices.cpp
@@ -0,0 +1,57 @@
+#include "../listasEnlazadas/matrizDispersa.h"
+
+int main()
+{
+    int cases;
+    cin >> cases;
+    int iteration = 1;
+    while (iteration <= cases)
+    {
+        int sizeMat;
+        cin >> sizeMat;
+        vector<vector<int>> mat;
+
+        for (int i = 0; i < sizeMat; i++)
+        {
+            string line;
+            cin >> line;
+            vector<int> newLine;
+            for (int j = 0; j < sizeMat; j++)
+            {
+                newLine.push_back(line[j] - 48);
+            }
+            mat.push_back(newLine);
+        }
+
+        DisperseMatrix matrizDispersa(mat, sizeMat, sizeMat);
+
+        int trCases;
+        cin >> trCases;
+        while (trCases--)
+        {
+            string tr;
+            cin >> tr;
+            if (tr == "row")
+            {
+                int a, b;
+                cin >> a >> b;
+                vector<pair<int, int>> rowA;
+                // cambio la fila a por la b CONTANDO DESDE 1
+
+            }
+        }
+
+
+
+        cout << "mat" << endl;
+        for (int i = 0; i < sizeMat; i++)
+        {
+            for (int j = 0; j < sizeMat; j++)
+            {
+                cout << mat[i][j] << " ";
+            }
+            cout << endl;
+        }
+        iteration++;
+    }
+}
